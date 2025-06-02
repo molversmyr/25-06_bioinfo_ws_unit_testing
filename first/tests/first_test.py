9e98824a2f1d3c5dc1bf260a4c30b9ccbda382f5
Håvard Molversmyr <54852797+molversmyr@users.noreply.github.com>
2025-06-02 15:43:36 +0000

    test: add test class and methods for 'greeting' function

--- first/tests/first_test.py
+++ first/tests/first_test.py
@@ -8,3 +8,17 @@ from first.first import greeting
 # if you like, add other languages to your greeting() function and test for them
 # add your changes: git add first/tests/first_test.py
 # commit your changes using commit message conventions (https://inpred.github.io/24-03_bioinfo_ws/#19): git commit -m "test: <your commit message>"
+
+class TestGreetingFunction(unittest.TestCase):
+    
+    def test_greeting_english(self):
+        self.assertEqual(greeting("John", "English"), "Hello John!")
+    
+    def test_greeting_norwegian(self):
+        self.assertEqual(greeting("Ola", "Norwegian"), "Hei Ola!")
+    
+    def test_greeting_unsupported_language(self):
+        self.assertEqual(greeting("Alice", "Spanish"), "I don't speak your language!")
+
+if __name__ == '__main__':
+    unittest.main()
